f0412fa2969a89a1ec5c28a9583a2b87f69615ef
sreekanthpaatil <sreekanthpaatil@gmail.com>
2022-12-05 22:21:44 +0530

    closure completed

--- Week-3/Assignment-3/closure.js
+++ Week-3/Assignment-3/closure.js
@@ -10,8 +10,20 @@ function createIncrement(){
     return [increment,log];
 }
 const [increment,log] = createIncrement();
-increment();    
-increment();   
-increment();    
-log();          
+increment();    // count ++// 1
+increment();   //  count ++//2
+increment();   // count ++ // 3 
+log();         // 0
 
+
+/* 
+Explaination:
+The expected output is 3 but the actual output is 0;
+This is because in log() we are printing the message and not the actual count.
+So when log() is called in line 16:
+line 8 is executed ->
+ message is on line 6 -> 
+ but in line 6 it is finding for count -> 
+ count is found on line2 in the lexical scope -> 
+ but its initialized at 0;
+*/
