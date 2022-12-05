5d6d59bfffce56d0bb71d283bfb3a62eb5701618
sreekanthpaatil <sreekanthpaatil@gmail.com>
2022-12-05 21:54:43 +0530

    bind call completed

--- Week-3/Assignment-2/callapplybind.js
+++ Week-3/Assignment-2/callapplybind.js
@@ -8,3 +8,25 @@ var person = {
 }
 
 person.hello("world");  // output is Sreekanth says hello world
+
+var helloFun = person.hello.bind({name : "Vaibhav"}); //bind attaches this into function and it needs to be invoked suparatly
+helloFun("world"); //  Output: Vaibhav says hello world
+
+//****************************
+
+//call
+
+var animal = {
+    name: "Bolt",
+    hello: function(thing) {
+        console.log(this.name + " is a " + thing);
+    }
+
+}
+
+animal.hello("dog"); //output: Bolt is a dog
+animal.hello.call({name: "Kitty"}, "cat"); // output: Kitty is a cat
+
+//call attaches this into function and executes the function immediatly
+
+//****************** */
